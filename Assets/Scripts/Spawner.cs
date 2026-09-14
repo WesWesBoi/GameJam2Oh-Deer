c9a3db9672ec514f0506460a3c6b759b9453ce50
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2026-09-13 20:18:20 -0400

    Created NPC Spawner script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject character;
+    public int maxSpawns = 3;
+    public int currentSpawns = 0;
+    public float delay = 1f;
+    public Transform maxX;
+    public Transform maxZ;
+    private float randomX;
+    private float randomZ;
+    public Transform spawnPosition;
+    public Quaternion Q; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentSpawns < maxSpawns)
+        {
+            if (delay > 0)
+            {
+                delay = delay - Time.deltaTime;
+            }
+            else 
+            {
+                selectRandomSpawnPoint();
+                currentSpawns = currentSpawns + 1;
+                Instantiate(character, spawnPosition.position, Q);
+                delay = 2f;
+            }
+        }
+    }
+
+    private void selectRandomSpawnPoint()
+    {
+        float distanceX = transform.position.x - maxX.position.x;
+        float distanceZ = transform.position.z - maxZ.position.z;
+
+        randomX = Random.Range((transform.position.x + distanceX), maxX.position.x);
+        randomZ = Random.Range((transform.position.z + distanceZ), maxZ.position.z);
+        spawnPosition.position = new Vector3(randomX, spawnPosition.position.y, randomZ);
+        Q = Quaternion.Euler(new Vector3(0, Random.Range(0, 361), 0));
+    }
+}
